60b6fd18b6fced7569049729173739dd2efff4dd
Maciej Czerwinski <maciejcz@wp.pl>
2017-03-29 22:09:44 +0200

    9.3

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function(){
+	var carouselList = $("#carousel ul");
+	
+
+	setInterval(function changeSlides() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	}, 3000);
+
+
+	function moveFirstSlide() {
+	var firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last");
+
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft: 0});
+	}
+});
